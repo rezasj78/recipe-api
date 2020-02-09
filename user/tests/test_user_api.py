c45fda16c6edd20f47609a20ac2e6fab90517708
Reza <sahraeianreza@gmail.com>
2020-02-09 11:46:38 +0330

    adde mange user endoint

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse("user:token")
+ME_URL = reverse('user:me')
 
 
 def crate_user(**params):
@@ -70,3 +71,43 @@ class publicUserApiTests(TestCase):
         response = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        response = self.client.get(ME_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTest(TestCase):
+    """requests that need authentication"""
+
+    def setUp(self):
+        self.user = crate_user(
+            email='somthing@gmail.com',
+            password='faskldjf',
+            name='name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrive_profile_success(self):
+        """retrieving profile for logged user"""
+        response = self.client.get(ME_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_not_allowed(self):
+        """test that post is not allowed"""
+        response = self.client.post(ME_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        payload = {'name': 'new name', 'password': 'f;ahdfjkhajkhdsflalks'}
+        response = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
